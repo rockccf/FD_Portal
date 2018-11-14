d50994aba9e831250428e968ec8807f041b08685
Chong Chee Fei <steven.chong@dbix.com.my>
2018-11-14 14:20:23 +0800

    Set to white color background color for some of the checkboxes in betting form.

--- app/scripts/angle.js
+++ app/scripts/angle.js
@@ -778,7 +778,7 @@
             name: 'TBT88 Portal',
             company: 'TBT88',
             description: 'Home',
-            version: '1.1.1',
+            version: '1.1.2',
             year: ((new Date()).getFullYear()),
             layout: {
                 isFixed: true,
