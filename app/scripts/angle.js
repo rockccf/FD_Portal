bfe54018a5ee4b325057ca5c28742b3a8b817c0c
Chong Chee Fei <steven.chong@dbix.com.my>
2018-12-25 00:19:02 +0800

    Bumped version to 1.2.1

--- app/scripts/angle.js
+++ app/scripts/angle.js
@@ -778,7 +778,7 @@
             name: 'TBT88 Portal',
             company: 'TBT88',
             description: 'Home',
-            version: '1.2.0',
+            version: '1.2.1',
             year: ((new Date()).getFullYear()),
             layout: {
                 isFixed: true,
