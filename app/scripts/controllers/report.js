a0702e6f93ad87ba835f18c0c24629965ac6418f
Chong Chee Fei <steven.chong@dbix.com.my>
2019-03-21 18:48:26 +0800

    Added bet amount number report. Changes made to display time on mobile view.

--- app/scripts/controllers/report.js
+++ app/scripts/controllers/report.js
@@ -9,6 +9,23 @@
         var _this = this; //Declare local variable _this to refer to "this" controller
         _this.isSaving = false; //Default to false
 
+        _this.big = null;
+        _this.small = null;
+        _this["3a"] = null;
+        _this["3b"] = null;
+        _this["3c"] = null;
+        _this["3d"] = null;
+        _this["3e"] = null;
+        _this["3f"] = null;
+        _this["3abc"] = null;
+        _this["4a"] = null;
+        _this["4b"] = null;
+        _this["4c"] = null;
+        _this["4d"] = null;
+        _this["4e"] = null;
+        _this["5d"] = null;
+        _this["6d"] = null;
+
         //Datepicker options
         _this.datepicker = {};
         _this.datepicker.format = 'dd-MMM-yyyy';
@@ -52,8 +69,25 @@
             reportParams.extraParams = {
                 "drawDateStart": moment(_this.drawDateStart).format(),
                 "drawDateEnd": moment(_this.drawDateEnd).format(),
-                "drawDate": moment(_this.drawDate).format()
+                "drawDate": moment(_this.drawDate).format(),
             };
+
+            _this.big ? reportParams.extraParams["big"] = _this.big : null;
+            _this.small ? reportParams.extraParams["small"] = _this.small : null;
+            _this["3a"] ? reportParams.extraParams["3a"] = _this["3a"] : null;
+            _this["3b"] ? reportParams.extraParams["3b"] = _this["3b"] : null;
+            _this["3c"] ? reportParams.extraParams["3c"] = _this["3c"] : null;
+            _this["3d"] ? reportParams.extraParams["3d"] = _this["3d"] : null;
+            _this["3e"] ? reportParams.extraParams["3e"] = _this["3e"] : null;
+            _this["3f"] ? reportParams.extraParams["3f"] = _this["3f"] : null;
+            _this["3abc"] ? reportParams.extraParams["3abc"] = _this["3abc"] : null;
+            _this["4a"] ? reportParams.extraParams["4a"] = _this["4a"] : null;
+            _this["4b"] ? reportParams.extraParams["4b"] = _this["4b"] : null;
+            _this["4c"] ? reportParams.extraParams["4c"] = _this["4c"] : null;
+            _this["4d"] ? reportParams.extraParams["4d"] = _this["4d"] : null;
+            _this["4e"] ? reportParams.extraParams["4e"] = _this["4e"] : null;
+            _this["5d"] ? reportParams.extraParams["5d"] = _this["5d"] : null;
+            _this["6d"] ? reportParams.extraParams["6d"] = _this["6d"] : null;
 
             $http.get($rootScope.SYSCONSTANT.BACKEND_SERVER_URL+"/report/get-report",{
                 params:reportParams
